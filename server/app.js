a6cefadd994180a91017845835aa89574536b4f8
marsan186 <marsan186@gmail.com>
2019-02-04 23:04:55 +0530

    login api added

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const path = require("path");
 const app = express();
 const mongoose = require('mongoose');
 const config = require("./config");
-
+const User = require("./model/user");
 mongoose.Promise = global.Promise;
 mongoose.connect(
     config.mongoURL,
@@ -13,6 +13,36 @@ mongoose.connect(
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../dist')));
+
+
+app.post("/register", function (req, res) {
+
+    const newUser = new User({
+        name: req.body.name,
+        username: req.body.user_name
+    });
+    newUser.password = newUser.generateHash(req.body.password);
+    newUser.save().then(rec => {
+        res.status(201).json(rec);
+    });
+
+});
+
+
+app.post("/login", function (req, res) {
+
+    User.findOne({ username: res.body.user_name }).then(loginUser => {
+        if (!loginUser) {
+            return res.status(401).json("Invalid user name and password");
+        }
+
+        if (!loginUser.validatePassword(req.body.password)) {
+            return res.status(401).json("Invalid password");
+        }
+        res.send(200).json(rec);
+    })
+
+});
 
 
 app.get("*", function (req, res) {
